744301718c1469110b1c2fc60043de92960f959e
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-11-24 13:05:27 -0500

    non angular websites test fix

--- configuration.js
+++ configuration.js
@@ -1,7 +1,7 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   //directConnect: true,
-  specs: ['element-basics.js'],
+  specs: ['spec1.js'],
   capabilities: {
     browserName: 'chrome',
   },
--- spec1.js
+++ spec1.js
@@ -1,5 +1,8 @@
 describe('Protractor Demo App', function () {
   it('should add one and two', function () {
+    browser.waitForAngularEnabled(false);
     browser.get('https://github.com/');
+    element(by.id('user[login]')).sendKeys('skmuddamsetty');
+    browser.sleep(5000);
   });
 });
