d5105ef34f5239caa248d03a46c40b1b3b440f39
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-11-24 10:38:51 -0500

    css binding

--- element-basics.js
+++ element-basics.js
@@ -1,11 +1,14 @@
 describe('element basics', () => {
   it('should behave...', () => {
     browser.get('http://juliemr.github.io/protractor-demo/');
-    element(by.model('first')).sendKeys('3');
-    element(by.model('second'))
-      .sendKeys('5')
-      .then(() => {
-        browser.sleep(5000);
+    element(by.model('first')).sendKeys(1);
+    element(by.model('second')).sendKeys(2);
+
+    element(by.id('gobutton')).click();
+    element(by.css("h2[class='ng-binding']"))
+      .getText()
+      .then((res) => {
+        console.log(res);
       });
   });
 });
